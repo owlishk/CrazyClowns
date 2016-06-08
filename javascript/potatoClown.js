a3179c7e83b383212d263a9efcd144d76efaf1fc
owlishk <ak314@yahoo.com>
2016-06-08 11:26:50 -0700

    potatoclown done

---
+++ javascript/potatoClown.js
@@ -0,0 +1,17 @@
+//greensocks draggable command
+
+Draggable.create("#eyes-scary-clown");
+Draggable.create("#mouth-scary-clown");
+Draggable.create("#hair-scary-clown");
+Draggable.create("#eyes-mime");
+Draggable.create("#mouth-mime");
+Draggable.create("#hair-mime");
+Draggable.create("#eyes-ted-cruz");
+Draggable.create("#mouth-ted-cruz");
+Draggable.create("#hair-ted-cruz");
+Draggable.create("#eyes-insane-posse");
+Draggable.create("#mouth-insane-posse");
+Draggable.create("#hair-insane-posse");
+Draggable.create("#eyes-happy-clown");
+Draggable.create("#mouth-happy-clown");
+Draggable.create("#hair-happy-clown");
